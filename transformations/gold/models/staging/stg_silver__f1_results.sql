d714b3763b732d6530ee57cc8222e77a22ac2355
dylanprole <d.p.prole@gmail.com>
2024-04-02 09:23:44 +1300

    Modified partition name

--- transformations/gold/models/staging/stg_silver__f1_results.sql
+++ transformations/gold/models/staging/stg_silver__f1_results.sql
@@ -10,17 +10,17 @@ renamed as (
 
     select
 
-        {{ adapter.quote("race_year") }},	
+        {{ adapter.quote("race_year") }} as season,	
         {{ adapter.quote("race_date") }},	
         {{ adapter.quote("season_round") }},	
         {{ adapter.quote("circuit_name") }},	
         {{ adapter.quote("position") }},	
         {{ adapter.quote("points") }},	
         {{ adapter.quote("grid_number") }},	
-        {{ adapter.quote("driver_name") }},	
+        {{ adapter.quote("driver_name") }} as driver,	
         {{ adapter.quote("driver_nationality") }},	
         {{ adapter.quote("driver_date_of_birth") }},	
-        {{ adapter.quote("constructor_name") }},	
+        {{ adapter.quote("constructor_name") }} as constructor,	
         {{ adapter.quote("constructor_nationality") }},	
         {{ adapter.quote("status_description") }},	
         {{ adapter.quote("fastest_lap_rank") }},	
